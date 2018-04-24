db412c428ddf450979096844818e691e0771ed48
Katharina Fey <kookie@spacekookie.de>
2018-04-24 21:57:53 +0200

    Working on I/O module

--- src/state/io.rs
+++ src/state/io.rs
@@ -1,76 +1,105 @@
-//! File interactions with project folder structures
-//!
-//! This provides easy to use utility functions to create, load
-//! and organise novelist projects.
-//!
-//! ### The folder structure is as follows
-//!
-//! - ProjectRoot
-//!   - ProjecName.novel (metadata file)
-//!   - Novel
-//!     - Chapters (multiple folders)
-//!       - Scene.bla (multiple files per folder)
-//!   - Universe (Internal)
-//!     - Internal.universe (metadata file)
-//!     - Assets
-//!     - ...
-//!   - Universe (Linked)
-//!     - ...
+//! Serailisation specific structures
 
 use serde_json;
-use std::error::Error;
-use std::io::Write;
-use std::{fs::{self, File},
-          path::Path};
+use std::fs::File;
+use std::io::{Read, Write, Error as IoError};
 
-use super::*;
+/// Represents the novel config on disk
+///
+/// This is a file with the `.novel` extention
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NovelData {
+    name: String,
+    author: String,
+    version: u8,
 
-impl Novel {
-    /// Creates a new skeleton project at a specific location
-    ///
-    /// If the path is `/path/to/folder` and the provided name is
-    /// `MyProject` then the resulting folder will be
-    /// `/path/to/folder/MyProject`.
-    ///
-    /// See the module documentation for described folder structure
-    ///
-    pub fn create(path: &str, name: &str, author: &str) -> Result<Novel, Box<Error>> {
-        let path = Path::new(path);
+    external_universe: Option<String>,
+}
 
-        /* Start with the root dir */
-        fs::create_dir_all(path.join(name))?;
-        fs::create_dir(path.join(name).join("Novel"))?;
-        fs::create_dir(path.join(name).join("Universe_Internal"))?;
+/// Represents the universe config on disk
+///
+/// This is a file with the `.universe` extention
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UniverseData {
+    name: String,
+    description: String,
+}
 
-        let universe = Universe {
-            name: String::from(name),
-            description: String::new(),
-            templates: Vec::new(),
-            characters: Vec::new(),
-            places: Vec::new(),
+impl NovelData {
+    /// Create a new novel metadata file on disk
+    pub fn create(name: String, author: String, path: &str) -> Option<NovelData> {
+        let mut f = match File::create(path) {
+            Ok(f) => f,
+            Err(_) => return None,
         };
 
-        let novel = Novel {
-            name: String::from(name),
-            author: String::from(author),
+        let me = NovelData {
+            name,
+            author,
             version: 0,
-            chapters: Vec::new(),
             external_universe: None,
-            internal_universe: LinkedUniverse::External(format!("{}", universe)),
         };
 
-        let novel_cereal = serde_json::to_string(&novel)?;
-        let mut novel_meta = File::create(path.join(name).join(&format!("{}.novel", name)))?;
-        novel_meta.write_all(novel_cereal.as_bytes())?;
+        f.write_all(serde_json::to_string(&me).unwrap().as_bytes())
+            .unwrap();
+        return Some(me);
+    }
 
-        let uni_cereal = serde_json::to_string(&universe)?;
-        let mut novel_meta = File::create(
-            path.join(name).join("Universe_Internal")
-                .join(&format!("{}.universe", name)),
-        )?;
-        novel_meta.write_all(uni_cereal.as_bytes())?;
+    /// Load an existing metadata file from disk
+    pub fn load(path: &str) -> Option<NovelData> {
+        return match File::open(path) {
+            Ok(ref mut f) => match f.get_string() {
+                Ok(c) => serde_json::from_str(&c).unwrap(),
+                Err(_) => None
+            },
+            _ => None
+        };
+    }
+}
 
-        /* Return an object that represents the new folders */
-        return Ok(novel);
+impl UniverseData {
+    /// Create a new novel metadata file on disk
+    pub fn create(name: String, description: String, path: &str) -> Option<UniverseData> {
+        let mut f = match File::create(path) {
+            Ok(f) => f,
+            Err(_) => return None,
+        };
+
+        let me = UniverseData {
+            name, description
+        };
+
+        f.write_all(serde_json::to_string(&me).unwrap().as_bytes())
+            .unwrap();
+        return Some(me);
+    }
+
+    /// Load an existing metadata file from disk
+    pub fn load(path: &str) -> Option<UniverseData> {
+        return match File::open(path) {
+            Ok(ref mut f) => match f.get_string() {
+                Ok(c) => serde_json::from_str(&c).unwrap(),
+                Err(_) => None
+            },
+            _ => None
+        };
+    }
+}
+
+/// A utility trait to read the conents from a file in
+/// a single line.
+pub trait FileToString {
+    /// Read the file contents into a string without any
+    /// error handling.
+    fn get_string(&mut self) -> Result<String, IoError>;
+}
+
+impl FileToString for File {
+    fn get_string(&mut self) -> Result<String, IoError> {
+        let mut s = String::new();
+        return match self.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e)
+        };
     }
 }
--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,34 +1,27 @@
 //! All the models used in Novelist
 
-pub(crate) mod io;
-
+mod io;
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
 /// A novel project is a collection of chapters and other metadata
-#[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct Novel {
     name: String,
     author: String,
     version: u8,
     chapters: Vec<Chapter>,
 
-    external_universe: Option<LinkedUniverse>,
-    internal_universe: LinkedUniverse,
+    external_universe: Option<Universe>,
+    internal_universe: Universe,
 }
 
-/// This is a bit of a hack.
-/// 
-/// Basically what it allows us to do is have a proper Novel -> Universe
-/// hierarchy in the structs while only saving references to the universe
-/// as a string. This is pretty shitty
-/// 
-/// Instead what we want to do is implement our own serialisers that
-/// take into account to only include a String of the name or path
-/// of the universe.
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) enum LinkedUniverse {
-    Internal(Universe),
-    External(String),
+/// A universe that has planning documents attached
+pub(crate) struct Universe {
+    name: String,
+    description: String,
+
+    templates: Vec<Document>,
+    characters: Vec<Document>,
+    places: Vec<Document>,
 }
 
 /// A chapter is a collection of scenes
@@ -40,49 +33,6 @@ pub(crate) struct Chapter {
     word_count: usize
 }
 
-/// A document in a novel or universe
-/// 
-/// This can either be a scene, a note, a template or an
-/// "implementation" of templates such as character sheets, etc
-/// 
-/// TODO: Add a schema type that allows to selectively allow
-/// typing (see external links)
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct Document {
-    name: String,
-    description: String,    
-    word_count: usize,
-    text: Vec<Sentence>
-}
-
-/// A complete text sentence that ends with a "." (normally)
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct Sentence {
-    snippets: Vec<TextSnippet>,
-    word_count: usize,
-    terminator: char
-}
-
-/// A piece of text with a formatting style attached to it
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct TextSnippet {
-    text: String,
-    word_count: usize,
-    style: TextStyle,
-}
-
-/// Describes a style of text
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) enum TextStyle {
-    /// Normally render this text
-    Plain,
-    /// Make it *italics*
-    Italics,
-    /// Make it **bold**
-    Bold,
-    /// Make it _underline_
-    Underline
-}
 
 /// The type of asset that is linked into a project
 #[derive(Debug, Serialize, Deserialize)]
@@ -100,19 +50,65 @@ pub(crate) struct Asset {
     blob: Vec<u8>
 }
 
-/// A universe project
-#[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct Universe {
-    name: String,
-    description: String,
 
-    templates: Vec<Document>,
-    characters: Vec<Document>,
-    places: Vec<Document>,
+/// A document in a novel or universe
+/// 
+/// This can either be a scene, a note, a template or an
+/// "implementation" of templates such as character sheets, etc
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Document {
+    name: String,
+    description: String,    
+    word_count: usize,
+    text: Vec<Paragraph>
 }
 
-impl Display for Universe {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        write!(f, "{}", self.name)
-    }
+/// A complete text Paragraph that ends with a "." (normally)
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Paragraph {
+    snippets: Vec<Sentence>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Sentence {
+    snippets: Vec<TextSnippet>,
+    terminator: char
+}
+
+/// A piece of text with a formatting style attached to it
+/// 
+/// ## Example
+/// 
+/// The simplest example would be
+/// 
+/// ```norun
+/// TextSnippet::new("Just some Text", TextStyle::Plain);
+/// ```
+/// 
+/// A more complicated example would immediately boil down to a 
+/// list of textsnippets, to make up a sentence:
+/// 
+/// ```norun
+/// let mut sentence = Vec::new();
+/// sentence.push(TextSnippet::new("Some", TextStyle::Plain));
+/// sentence.push(TextSnippet::new("amazing", TextStyle::Italics));
+/// sentence.push(TextSnippet::new("text!!!", TextStyle::Bold));
+/// ```
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct TextSnippet {
+    text: String,
+    style: TextStyle,
+}
+
+/// Describes a style of text
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) enum TextStyle {
+    /// Normally render this text
+    Plain,
+    /// Make it *italics*
+    Italics,
+    /// Make it **bold**
+    Bold,
+    /// Make it _underline_
+    Underline
 }
