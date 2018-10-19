78c679d1c808d228c8cb5ccc828e48b7e792c6d2
Katharina Fey <kookie@spacekookie.de>
2018-10-20 01:32:57 +0200

    Adding a textview that is scaled properly and can hold some text!
    Co-authored-by: Katharina Fey <kookie@spacekookie.de>
    Co-authored-by: Martin Holzhauer <martin@holzhauer.eu>

--- novelist-gtk/src/widgets/textview.rs
+++ novelist-gtk/src/widgets/textview.rs
@@ -1,8 +1,8 @@
 use gtk::prelude::*;
 use gtk::Orientation::{Horizontal, Vertical};
 use gtk::{
-    ButtonExt, GtkWindowExt, HeaderBarExt, Inhibit, LabelExt, OrientableExt, TextBufferExt,
-    TextViewExt, WidgetExt,
+    BoxExt, BuildableExt, ButtonExt, ContainerExt, GtkWindowExt, HeaderBarExt, Inhibit, LabelExt,
+    OrientableExt, ScrolledWindowExt, TextBufferExt, TextViewExt, WidgetExt,
 };
 use relm::{timeout, Relm, Widget};
 use relm_attributes::widget;
@@ -23,9 +23,34 @@ impl Widget for TextView {
     fn update(&mut self, e: Event) {}
 
     view! {
-        #[name="textview"]
-        gtk::TextView {
-            
+        #[name = "container"]
+        gtk::Box {
+            child: {
+                fill: true,
+                expand: true,
+            },
+            gtk::ScrolledWindow {
+                shadow_type: gtk::ShadowType::In,
+                property_width_request: 600,
+
+                #[name = "textview"]
+                gtk::TextView {
+                    margin_left: 25,
+                    margin_right: 25,
+                    margin_top: 25,
+                    margin_bottom: 25,
+                    pixels_below_lines: 25,
+                    pixels_inside_wrap: 15,
+                    wrap_mode: gtk::WrapMode::Word,
+                    hscroll_policy: gtk::ScrollablePolicy::Natural,
+                    left_margin: 50,
+                    right_margin: 50,
+                    top_margin: 75,
+                    bottom_margin: 75,
+                    indent: 50,
+                    accepts_tab: false,
+                }
+            }
         }
     }
 }
